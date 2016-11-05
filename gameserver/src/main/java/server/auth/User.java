14def7e6f3a2782a3e9c2068fe915961e1f74716
Pasheza <pashezik@gmail.com>
2016-11-06 02:15:41 +0300

    Changes in User class

--- gameserver/src/main/java/server/auth/User.java
+++ gameserver/src/main/java/server/auth/User.java
@@ -4,14 +4,34 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.NotNull;
 
+import javax.persistence.*;
+import java.util.Date;
+import java.util.concurrent.ThreadLocalRandom;
 /**
  * Created by Alex on 24.10.2016.
  */
+@Entity
+@Table(name = "users")
 public class User {
     @NotNull
     private static final Logger log = LogManager.getLogger(User.class);
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
     @NotNull
     private String name;
+
+    @Embedded
+    private Date date;
+
+    private String password;
+
+    @Column(nullable = true)
+    private String email;
+
+    private int tokenId;
 
     /**
      * Create new User
@@ -20,6 +40,7 @@ public class User {
      */
     public User(@NotNull String name) {
         this.name = name;
+        this.date = new Date();
         if (log.isInfoEnabled()) {
             log.info(toString() + " created");
         }
@@ -29,9 +50,25 @@ public class User {
         name = newName;
     }
 
+    public void setPassword(String newPassword) { this.password = newPassword; }
+
+    public void setEmail(String newEmail) { this.email = newEmail; }
+
+    public void setTokenId (int newTokenId) { this.tokenId = newTokenId; }
+
+    public int getId () { return this.id; }
+
     public String getName(){
         return name;
     }
+
+    public Date getDate() { return this.date; }
+
+    public String getPassword() { return this.password; }
+
+    public String getEmail() { return this.email; }
+
+    public int getTokenId() { return this.tokenId; }
 
     @Override
     public String toString() {
